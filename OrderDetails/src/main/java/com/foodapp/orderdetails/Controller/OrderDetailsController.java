23d52bd57fb03b0e59774cacbe80cd819622aa31
teammates77 <priyankaroyal2507@gmail.com>
2024-04-10 12:03:04 +0530

    added CROS origins

--- OrderDetails/src/main/java/com/foodapp/orderdetails/Controller/OrderDetailsController.java
+++ OrderDetails/src/main/java/com/foodapp/orderdetails/Controller/OrderDetailsController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +23,8 @@ import com.foodapp.orderdetails.model.OrderDetails;
 import com.foodapp.orderdetails.model.OrderItem;
 import com.foodapp.orderdetails.service.OrderDetailsService;
 
-
+@Validated
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/fooddelivery/orderdetails")
 public class OrderDetailsController {
